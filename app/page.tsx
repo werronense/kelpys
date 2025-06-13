4e21086976f62fce1c3abdc1d8c1eaa20fea3da9
Stephen Werronen <stephen.werronen@gmail.com>
2025-06-13 10:07:33 -0700

    Add pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-   <div>
-     <main>
-       <h1>Kelpy's</h1>
-     </main>
-   </div>
+    <div>
+      <main>
+        <h1>Kelpy's</h1>
+        <p>
+          <Link href={"/zod-basics"}>Zod</Link>
+        </p>
+        <p>
+          <Link href={"/form-defaults"}>Form with Default Error Messages</Link>
+        </p>
+        <p>
+          <Link href={"/form-custom"}>Form with Custom Error Messages</Link>
+        </p>
+      </main>
+    </div>
   );
 }
