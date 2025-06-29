af2d252f4434778c50a8beeb5151394964b00eda
Stephen Werronen <stephen.werronen@gmail.com>
2025-06-29 14:48:44 -0700

    Add phone number regex

--- app/custom-errors-schema.ts
+++ app/custom-errors-schema.ts
@@ -1,10 +1,14 @@
 import { z } from "zod/v4";
 
+const phoneNumberRegex = /^\(?\d{3}\)?\s?-?\d{3}\s?-?\d{4}$/;
+
 // validation schema with custom error messages
 export const customErrorsSchema = z.object({
   name: z.string({ error: "Name is required" }),
   email: z.email({ error: "Email is required" }),
-  phone: z.string({ error: "Phone is required" }),
+  phone: z
+    .string({ error: "Phone is required" })
+    .regex(phoneNumberRegex, { error: "Enter a 10-digit phone number" }),
 });
 
 export type CustomErrorsSchemaT = z.infer<typeof customErrorsSchema>;
--- app/default-errors-schema.ts
+++ app/default-errors-schema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod/v4";
 
+const phoneNumberRegex = /^\(?\d{3}\)?\s?-?\d{3}\s?-?\d{4}$/;
+
 // basic validation schema with default error messages
 export const defaultErrorsSchema = z.object({
   name: z.string(),
   email: z.email(),
-  phone: z.string(),
+  phone: z.string().regex(phoneNumberRegex),
 });
 
 export type DefaultErrorsSchemaT = z.infer<typeof defaultErrorsSchema>;
