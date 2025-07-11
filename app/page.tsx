3a438c2194accd3c2a31b2000dbf7502dbe4bb87
Stephen Werronen <stephen.werronen@gmail.com>
2025-07-11 11:13:01 -0700

    Add outline to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
+import PageHeading from "@/components/page-heading";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>Kelpy's</h1>
-        <p>
-          <Link href={"/zod-basics"}>Zod</Link>
-        </p>
-        <p>
-          <Link href={"/form-defaults"}>Form with Default Error Messages</Link>
-        </p>
-        <p>
-          <Link href={"/form-custom"}>Form with Custom Error Messages</Link>
-        </p>
-      </main>
+    <div className={"space-y-4"}>
+      <PageHeading text={"Form Validation and Error Messages with Zod"} />
+      <p className={"text-xl"}>My talk tonight has 3 parts:</p>
+      <ol className={"list-decimal text-lg pl-4 space-y-3"}>
+        <li>
+          <Link href={"/zod-basics"}>Zod basics</Link>
+        </li>
+        <li>
+          <Link href={"/form-defaults"}>Form validation with Zod</Link>
+        </li>
+        <li>
+          <Link href={"/form-custom"}>Customizing Error Messages</Link>
+        </li>
+      </ol>
     </div>
   );
 }
