b6a72869d63a564bef70f7f6f910425b885ddadc
Stephen Werronen <stephen.werronen@gmail.com>
2025-07-11 11:32:15 -0700

    Add content to Zod page

--- app/zod-basics/page.tsx
+++ app/zod-basics/page.tsx
@@ -1,3 +1,20 @@
+import PageHeading from "@/components/page-heading";
+import Link from "next/link";
+
 export default function Page() {
-    return <h1>Zod</h1>
+  return (
+    <div className={"space-y-4"}>
+      <PageHeading text={"What is Zod?"} />
+      <section className={"p-4 space-y-4"}>
+        <blockquote cite={"https://zod.dev/"}>
+          "Zod is a TypeScript-first validation library. Using Zod, you can
+          define schemas you can use to validate data, from a simple string to a
+          complex nested object."
+        </blockquote>
+        <cite className={"flex justify-end"}>
+          <Link href="https://zod.dev/">Zod website</Link>
+        </cite>
+      </section>
+    </div>
+  );
 }
