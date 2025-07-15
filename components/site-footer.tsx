4489ffab3a44ac492b709d05b63e452a70b77a2d
Stephen Werronen <stephen.werronen@gmail.com>
2025-07-14 17:26:47 -0700

    Adjust site footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,7 +1,7 @@
 export default function SiteFooter() {
   return (
     <footer className={"bg-foreground text-background py-6 px-4 w-full"}>
-      <div className={"max-w-sm mx-auto sm:px-4"}>
+      <div className={"flex justify-end max-w-sm mx-auto sm:px-4"}>
         <small>&copy; Stephen Werronen 2025</small>
       </div>
     </footer>
